d046757d57c9f2cf67bac3bbdc53203e0cf50c8e
Aurélien R <rodier.aurelien@orange.fr>
2025-02-24 11:42:36 +0100

    Remove deprecated files and configurations, and add new flipbook styles

--- app/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .dependency_manager import DependencyManager
-from .html_generator import HTMLGenerator
-from .pdf_processor import PDFProcessor
-
-__all__ = ["PDFProcessor", "HTMLGenerator", "DependencyManager"]
-
-print("Utils chargés avec succès!")
